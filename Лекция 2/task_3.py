00ee456141d6a9a120274576e663617f9450090e
nsh12 <ansh120022@gmail.com>
2020-12-10 17:36:30 +0300

    add  list_comprehension

--- Лекция 2/task_3.py
+++ Лекция 2/task_3.py
@@ -1,3 +1,7 @@
+required_files = ['README.txt', 'scenario.json', 'config.hat']
+all_files = ['bla.html', 'lala.xsl']
+
+#option1
 def check_existing_files(required_files, all_files):
     missed_required_files = []
     for x in range(len(required_files)):
@@ -7,8 +11,11 @@ def check_existing_files(required_files, all_files):
     for i in range(len(missed_required_files)):
         print(f"{missed_required_files[i]}\n")
 
+#option2
+def use_list_comprehension(required_files, all_files):
+    missed_required_files = [i for i in required_files if i not in all_files]
+    print(missed_required_files)
 
-required_files = ['README.txt', 'scenario.json', 'config.hat']
-all_files = ['bla.html', 'lala.xsl']
 
 check_existing_files(required_files, all_files)
+use_list_comprehension(required_files, all_files)
